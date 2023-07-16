f92e9a2f11daeedf4c08aea56fe02be6321981d2
Jesus Puentes <jopt98@gmail.com>
2023-07-16 10:58:47 -0500

    Se añadió el query persistence al searchdelegate

--- twelve_cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
+++ twelve_cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,17 +1,16 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:twelve_cinemapedia/domain/entities/movie.dart';
 import 'package:twelve_cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:twelve_cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:twelve_cinemapedia/presentation/providers/search/search_movies_provider.dart';
 
 class CustomAppBar extends ConsumerWidget {
   const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
 
@@ -24,32 +23,38 @@ class CustomAppBar extends ConsumerWidget {
           width: double.infinity,
           child: Row(
             children: [
-              Icon( Icons.movie_outlined , color: colors.primary),
+              Icon(Icons.movie_outlined, color: colors.primary),
               const SizedBox(width: 5),
-              Text('Cinemapedia', style: titleStyle,),
-
+              Text(
+                'Cinemapedia',
+                style: titleStyle,
+              ),
               const Spacer(),
-
               IconButton(
                 onPressed: () {
+                  final movieRepository = ref.read(movieRepositoryProvider);
+                  final searchQuery = ref.read(searchQueryProvider);
 
-                  final movieRepository = ref.read( movieRepositoryProvider );
-                  
                   final movie = showSearch<Movie?>(
+                    query: searchQuery,
                     context: context,
                     delegate: SearchMovieDelegate(
-                      searchMovies: movieRepository.searchMovies,
+                      searchMovies: (query) {
+                        ref
+                            .read(searchQueryProvider.notifier)
+                            .update((state) => query);
+                        return movieRepository.searchMovies(query);
+                      },
                     ),
                   ).then(
                     (movie) {
-                      if( movie == null ) return;
+                      if (movie == null) return;
 
-                      context.push('/movie/${ movie.id }');
+                      context.push('/movie/${movie.id}');
                     },
                   );
-
                 },
-                icon: const Icon( Icons.search ),
+                icon: const Icon(Icons.search),
               )
             ],
           ),
